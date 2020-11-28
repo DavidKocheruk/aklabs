0b8266dfae726ac6e5175f7c940b76505478a8d3
DavidKocheruk <david.kocheruk@gmail.com>
2020-11-28 17:00:36 +0200

    Made 15 changes

--- calculator.cpp
+++ calculator.cpp
@@ -2,15 +2,24 @@
 
 int Calculator::Add (double a, double b)
 {
-	return a + b + 0.5;
+	return a + b + 3.5;
+	// comment in Add
 }
 
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
+    // comment in Sub
 }
 
 int Calculator::Mul (double a, double b)
 {
-    return a * b + 0.5;
+    return a * b;
+    // comment in Mul
+}
+
+int Calculator::Div (double a, double b)
+{
+	return a / b;
+	// comment in Div
 }
